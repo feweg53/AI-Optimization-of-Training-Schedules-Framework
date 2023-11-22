cf39222ccbe7baf18056ef3ccb0c20d852d1a9e4
Cecey <72633487+feweg53@users.noreply.github.com>
2023-11-22 02:39:43 -0600

    Update 5_Server.py

--- 5_Server.py
+++ 5_Server.py
@@ -7,38 +7,39 @@ import joblib
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)  # Enable Cross-Origin Resource Sharing
 
 # Load the model. Ensure this is the correct model file.
-model = joblib.load('optimized_random_forest_model.pkl')  
+model = joblib.load('optimized_random_forest_model.pkl')
 
 def preprocess_input(data):
     """
     Preprocesses the input data to match the model's input format.
-    Convert the incoming JSON data into the format expected by the model.
     """
     df = pd.DataFrame([data])
 
-    # Convert time columns to datetime and extract hours
-    df['scheduled_departure_time'] = pd.to_datetime(df['scheduled_departure_time'])
-    df['scheduled_arrival_time'] = pd.to_datetime(df['scheduled_arrival_time'])
-    df['scheduled_departure_hour'] = df['scheduled_departure_time'].dt.hour
-    df['scheduled_arrival_hour'] = df['scheduled_arrival_time'].dt.hour
-    df['departure_hour'] = pd.to_datetime(df['departureTime']).dt.hour
-    df['arrival_hour'] = pd.to_datetime(df['arrivalTime']).dt.hour
+    # Handle datetime conversion and hour extraction
+    try:
+        df['scheduled_departure_hour'] = pd.to_datetime(df['scheduled_departure_time']).dt.hour
+        df['scheduled_arrival_hour'] = pd.to_datetime(df['scheduled_arrival_time']).dt.hour
+        df['departure_hour'] = pd.to_datetime(df['departureTime']).dt.hour
+        df['arrival_hour'] = pd.to_datetime(df['arrivalTime']).dt.hour
+    except KeyError as e:
+        print(f"Missing key in data: {e}")
+        return None
 
-    # Encode categorical variables and scale numerical columns
+    # Encode and scale data
     label_encoder = LabelEncoder()
     scaler = MinMaxScaler()
-    
-    # Assuming 'weatherCondition' needs encoding
-    df['weather_condition_encoded'] = label_encoder.fit_transform(df['weather_condition'])
-    
-    # Scaling numerical columns
-    numerical_cols = ['scheduled_departure_hour', 'scheduled_arrival_hour']
-    df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
+    try:
+        df['weather_condition_encoded'] = label_encoder.fit_transform(df['weather_condition'])
+        numerical_cols = ['scheduled_departure_hour', 'scheduled_arrival_hour', 'departure_hour', 'arrival_hour']
+        df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
+    except KeyError as e:
+        print(f"Missing key for encoding/scaling: {e}")
+        return None
 
-    # Select the columns used by the model
+    # Columns used by the model
     model_cols = ['weather_condition_encoded', 'scheduled_departure_hour', 'scheduled_arrival_hour']
     return df[model_cols]
 
@@ -46,23 +47,30 @@ def preprocess_input(data):
 def predict():
     data = request.json
     processed_data = preprocess_input(data)
-    
-    # Make prediction
-    prediction = model.predict(processed_data)
-    
-    # Format for Chart.js (example for a bar chart)
-    chart_data = {
-        'labels': ['Predicted Delay'],  # x-axis labels
-        'datasets': [{
-            'label': 'Delay in Minutes',
-            'data': prediction.tolist(),  # y-axis data
-            'backgroundColor': ['rgba(255, 99, 132, 0.2)'],  # Colors for bars
-            'borderColor': ['rgba(255, 99, 132, 1)'],
-            'borderWidth': 1
-        }]
-    }
-    
-    return jsonify({'prediction': prediction.tolist()})
+
+    if processed_data is not None:
+        # Make prediction
+        try:
+            prediction = model.predict(processed_data)
+        except Exception as e:
+            print(f"Error during prediction: {e}")
+            return jsonify({'error': 'Error during prediction'}), 500
+
+        # Format for Chart.js visualization
+        chart_data = {
+            'labels': ['Predicted Delay'],
+            'datasets': [{
+                'label': 'Delay in Minutes',
+                'data': prediction.tolist(),
+                'backgroundColor': ['rgba(255, 99, 132, 0.2)'],
+                'borderColor': ['rgba(255, 99, 132, 1)'],
+                'borderWidth': 1
+            }]
+        }
+        
+        return jsonify({'chartData': chart_data})
+    else:
+        return jsonify({'error': 'Invalid input data'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #set debug=False for production deployment
+    app.run(debug=True, port=5000)
