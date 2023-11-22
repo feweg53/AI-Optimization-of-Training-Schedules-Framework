3f1c74c6c13f88cdc029b879ae166dd64ba2619a
Cecey <72633487+feweg53@users.noreply.github.com>
2023-11-22 00:48:14 -0600

    Update 5_Server.py

--- 5_Server.py
+++ 5_Server.py
@@ -22,11 +22,17 @@ def preprocess_input(data):
     df['scheduled_arrival_time'] = pd.to_datetime(df['scheduled_arrival_time'])
     df['scheduled_departure_hour'] = df['scheduled_departure_time'].dt.hour
     df['scheduled_arrival_hour'] = df['scheduled_arrival_time'].dt.hour
+    df['departure_hour'] = pd.to_datetime(df['departureTime']).dt.hour
+    df['arrival_hour'] = pd.to_datetime(df['arrivalTime']).dt.hour
 
     # Encode categorical variables and scale numerical columns
     label_encoder = LabelEncoder()
     scaler = MinMaxScaler()
+    
+    # Assuming 'weatherCondition' needs encoding
     df['weather_condition_encoded'] = label_encoder.fit_transform(df['weather_condition'])
+    
+    # Scaling numerical columns
     numerical_cols = ['scheduled_departure_hour', 'scheduled_arrival_hour']
     df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
 
@@ -41,6 +47,18 @@ def predict():
     
     # Make prediction
     prediction = model.predict(processed_data)
+    
+    # Format for Chart.js (example for a bar chart)
+    chart_data = {
+        'labels': ['Predicted Delay'],  # x-axis labels
+        'datasets': [{
+            'label': 'Delay in Minutes',
+            'data': prediction.tolist(),  # y-axis data
+            'backgroundColor': ['rgba(255, 99, 132, 0.2)'],  # Colors for bars
+            'borderColor': ['rgba(255, 99, 132, 1)'],
+            'borderWidth': 1
+        }]
+    }
     
     return jsonify({'prediction': prediction.tolist()})
 
