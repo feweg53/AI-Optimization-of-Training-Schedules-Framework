d3148446f12ede386a2d95e51aae65b5f1124342
Cecey <72633487+feweg53@users.noreply.github.com>
2023-11-21 23:38:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 //script.js
 
-document.getElementById("predictionForm").addEventListener("submit", function(event){
+document.getElementById("predictionForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    let departureTime = document.getElementById("departureTime").value;
-    let weatherCondition = document.getElementById("weatherCondition").value;
-    let crewOnStandby = document.getElementById("crewOnStandby").value;
+    // Collect all necessary form inputs
+    let formData = {
+        departureTime: document.getElementById("departureTime").value,
+        weatherCondition: document.getElementById("weatherCondition").value,
+        crewOnStandby: document.getElementById("crewOnStandby").value,
+        aircraftType: document.getElementById("aircraftType").value,
+        destinationAirport: document.getElementById("destinationAirport").value,
+        departureAirport: document.getElementById("departureAirport").value,
+        departureCityCode: document.getElementById("departureCityCode").value,
+        fullRouting: document.getElementById("fullRouting").value,
+        generalEquipmentType: document.getElementById("generalEquipmentType").value,
+        elapsedJourneyTime: document.getElementById("elapsedJourneyTime").value,
+        scheduleFingerprint: document.getElementById("scheduleFingerprint").value,
+        passengerClasses: document.getElementById("passengerClasses").value,
+        // ... continue for all other fields
+    };
 
     let url = "http://localhost:5000/predict";
 
     fetch(url, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 
-            departureTime: departureTime, 
-            weatherCondition: weatherCondition, 
-            crewOnStandby: crewOnStandby 
-        }),
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
     })
     .then(response => {
-        if(response.ok) {
+        if (response.ok) {
             return response.json();
         } else {
             throw new Error('Server response was not OK');
@@ -32,6 +39,7 @@ document.getElementById("predictionForm").addEventListener("submit", function(ev
     })
     .catch((error) => {
         console.error('Error:', error);
-        document.getElementById("predictionResult").innerHTML = "Error in prediction: " + error;
+        document.getElementById("predictionResult").innerHTML = "Error in prediction: " + error.message;
     });
 });
+
