8d5292f521d2c05dba9bee6633926e38f4b556ea
Cecey <72633487+feweg53@users.noreply.github.com>
2023-11-21 23:27:17 -0600

    Update 6_RealTimeDataProcessing.py

--- 6_RealTimeDataProcessing.py
+++ 6_RealTimeDataProcessing.py
@@ -4,6 +4,8 @@ import requests
 import time
 import json
 import logging
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 # Configure logging
 logging.basicConfig(filename='realtime_data_processing.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
@@ -31,9 +33,21 @@ def process_data(data):
     :param data: The real-time data to be processed.
     :return: Processed data.
     """
-    # Example: Process data based on your requirements
-    # processed_data = ...
-    return processed_data
+    # Convert relevant columns to datetime
+    data['scheduled_departure_time'] = pd.to_datetime(data['scheduled_departure_time'])
+    data['scheduled_arrival_time'] = pd.to_datetime(data['scheduled_arrival_time'])
+
+    # Extract features used by the model
+    data['scheduled_departure_hour'] = data['scheduled_departure_time'].dt.hour
+    data['scheduled_arrival_hour'] = data['scheduled_arrival_time'].dt.hour
+
+    # Encode categorical variables and scale numerical columns as per model requirements
+    label_encoder = LabelEncoder()
+    scaler = MinMaxScaler()
+    data['weather_condition_encoded'] = label_encoder.fit_transform(data['weather_condition'])
+    data[['scheduled_departure_hour', 'scheduled_arrival_hour']] = scaler.fit_transform(data[['scheduled_departure_hour', 'scheduled_arrival_hour']])
+    
+    return data
 
 def send_to_scheduling_system(processed_data, scheduling_system_endpoint):
     """
@@ -43,10 +57,30 @@ def send_to_scheduling_system(processed_data, scheduling_system_endpoint):
     :param scheduling_system_endpoint: Endpoint of the scheduling system.
     """
     try:
-        response = requests.post(scheduling_system_endpoint, json=processed_data)
-        logging.info(f"Data sent to scheduling system. Status Code: {response.status_code}")
+        # Generate predictions
+        predictions = model.predict(processed_data)
+        response = requests.post(scheduling_system_endpoint, json={'predictions': predictions.tolist()})
+
+        if response.status_code == 200:
+            logging.info("Predictions successfully sent to the scheduling system.")
+        else:
+            logging.error(f"Failed to send predictions. Status Code: {response.status_code}")
     except Exception as e:
-        logging.error(f"Error sending data to scheduling system: {e}")
+        logging.error(f"Error in sending predictions: {e}")
+
+def main(model_path, source_url, scheduling_system_endpoint):
+    # Load the trained model
+    model = joblib.load(model_path)
+
+    while True:
+        # Fetch and process real-time data
+        data = fetch_realtime_data(source_url)
+        if data:
+            processed_data = preprocess_realtime_data(data)
+            send_to_scheduling_system(processed_data, scheduling_system_endpoint, model)
+
+        # Fetch new data at regular intervals (e.g., every 10 seconds)
+        time.sleep(10)
 
 def main():
     source_url = 'http://realtime_data_source_url'
@@ -65,4 +99,7 @@ def main():
         time.sleep(10)
 
 if __name__ == '__main__':
-    main()
+    model_path = 'path_to_trained_model.pkl'
+    source_url = 'http://realtime_data_source_url'
+    scheduling_system_endpoint = 'http://scheduling_system_endpoint'
+    main(model_path, source_url, scheduling_system_endpoint)
