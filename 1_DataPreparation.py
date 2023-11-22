574425baadfeb94e5494d43eda807545a97a9c68
Cecey <72633487+feweg53@users.noreply.github.com>
2023-11-22 02:45:35 -0600

    Update 1_DataPreparation.py

--- 1_DataPreparation.py
+++ 1_DataPreparation.py
@@ -30,17 +30,18 @@ def preprocess_data(data):
     :param data: DataFrame to preprocess.
     :return: Preprocessed DataFrame.
     """
+    # Fill missing values
     data.fillna(method='ffill', inplace=True)
-    
+
     # Convert time columns to datetime format
     time_cols = ['scheduled_departure_time', 'scheduled_arrival_time', 'departure_time', 'arrival_time']
     for col in time_cols:
         data[col] = pd.to_datetime(data[col])
-        
-    data['scheduled_departure_time'] = pd.to_datetime(data['scheduled_departure_time'])
-    data['scheduled_arrival_time'] = pd.to_datetime(data['scheduled_arrival_time'])
-    data['departure_hour'] = data['scheduled_departure_time'].dt.hour
-    data['arrival_hour'] = data['scheduled_arrival_time'].dt.hour
+
+    # Extract hours from time columns
+    for col in time_cols:
+        data[f'{col}_hour'] = data[col].dt.hour
+
     return data
 
 def handle_uncertainties(data):
@@ -51,33 +52,25 @@ def handle_uncertainties(data):
     :param data: DataFrame containing the schedule data.
     :return: Adjusted DataFrame.
     """
-    # Example: Adjust flight times based on weather conditions
-    # Assuming there's a 'weather_condition' column and it affects flight duration
-    # Can adjust this logic based on your specific data and requirements
-
-    # Define a simple mapping of weather conditions to delay factor
+    # Define a mapping of weather conditions to delay factor
     weather_delay_factor = {
-        'clear': 1.0,   # no delay
-        'rainy': 1.1,   # 10% more time
-        'stormy': 1.2,  # 20% more time
-        'foggy': 1.15,  # 15% more time
+        'clear': 1.0, 'rainy': 1.1, 'stormy': 1.2, 'foggy': 1.15
     }
 
-    # Apply the delay factor to the flight duration
+    # Apply delay factor to flight duration
     data['adjusted_flight_duration'] = data.apply(
         lambda row: row['flight_duration'] * weather_delay_factor.get(row['weather_condition'], 1),
         axis=1
     )
 
-    # Example: Handling crew availability
-    # Assuming a column 'crew_on_standby' indicating if extra crew is available
-    # Adjusting flight status based on crew availability
+    # Adjust flight status based on crew availability
     data['flight_status'] = data.apply(
         lambda row: 'delayed' if row['crew_on_standby'] < 3 and row['weather_condition'] == 'stormy' else 'on_time',
         axis=1
     )
 
     return data
+
 
 def encode_and_scale(data, categorical_columns, numerical_columns):
     """
@@ -88,45 +81,44 @@ def encode_and_scale(data, categorical_columns, numerical_columns):
     :param numerical_columns: List of names of numerical columns.
     :return: DataFrame with encoded and scaled data.
     """
+  # Encoding categorical columns
     label_encoder = LabelEncoder()
     for col in categorical_columns:
         data[col] = label_encoder.fit_transform(data[col])
+
+    # Scaling numerical columns
     scaler = MinMaxScaler()
     data[numerical_columns] = scaler.fit_transform(data[numerical_columns])
+
     return data
+
 
 # Main execution
 if __name__ == "__main__":
     try:
-        # Load the data
+        # Load and preprocess data
         data = load_data('your_dataset.csv')
         if data is None:
-            raise ValueError("No data loaded. Check if the file exists or is valid.")
+            raise ValueError("Data file not found or invalid.")
 
-        # Data preprocessing
         data = preprocess_data(data)
-        logging.info("Data preprocessing completed.")
         data = handle_uncertainties(data)
 
         # Encoding and scaling
-        categorical_columns = ['weather_condition', 'aircraft_type', 'destination_airport', 'departure_airport']
-        numerical_columns = ['scheduled_departure_hour', 'scheduled_arrival_hour', 'actual_departure_hour', 'actual_arrival_hour', 'crew_on_standby', 'flight_duration', 'adjusted_flight_duration']
-        data = encode_and_scale(data, categorical_columns, numerical_columns)
-        logging.info("Encoding and scaling of data completed.")
+        categorical_cols = ['weather_condition', 'aircraft_type', 'destination_airport', 'departure_airport']
+        numerical_cols = ['scheduled_departure_time_hour', 'scheduled_arrival_time_hour', 'departure_time_hour', 'arrival_time_hour', 'crew_on_standby', 'flight_duration', 'adjusted_flight_duration']
+        data = encode_and_scale(data, categorical_cols, numerical_cols)
 
-        # Splitting the data
+        # Splitting data
         X = data.drop('delay_minutes', axis=1)
         y = data['delay_minutes']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        logging.info("Data split into training and testing sets.")
 
-        # Saving the processed data
+        # Saving processed data
         X_train.to_csv('X_train.csv', index=False)
         X_test.to_csv('X_test.csv', index=False)
         y_train.to_csv('y_train.csv', index=False)
         y_test.to_csv('y_test.csv', index=False)
-        logging.info("Processed data saved successfully.")
 
     except Exception as e:
-        logging.error(f"An error occurred during the main execution: {e}")
-
+        logging.error(f"An error occurred: {e}")
